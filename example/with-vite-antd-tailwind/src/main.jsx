5b52ba8edb6485fe4d014e7ae300244186c03050
Chinnawat Chimdee <chinnawat.cpre@gmail.com>
2023-04-11 11:48:38 +0700

    [Added] - Vercel Analytics for example project

--- example/with-vite-antd-tailwind/src/main.jsx
+++ example/with-vite-antd-tailwind/src/main.jsx
@@ -1,7 +1,10 @@
+import { inject } from "@vercel/analytics";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
+
+inject();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
